c1569f95a34cb23479efcea9e15b7c95920cdd2f
saguilar4891 <amsebastian12@gmail.com>
2026-02-21 14:28:13 -0500

    Added sine patterns

--- Scrips/Player/Crosshair.cs
+++ Scrips/Player/Crosshair.cs
@@ -4,16 +4,25 @@ using System.Security.Cryptography.X509Certificates;
 
 public partial class Crosshair : Control
 {
-	[Export] public float Radius = 20f;
-    [Export] public float Thickness = 2f;
-    [Export] public Color CrosshairColor = Colors.White;
-    [Export] public int Segments = 64;
-
+    [ExportGroup("Crosshair Settings")]
+	[Export] private float Radius = 20f;
+    [Export] private float Thickness = 2f;
+    [Export] private Color CrosshairColor = Colors.White;
+    [Export] private int Segments = 64;
+   
+    private SinePatterns sinePattern;
+    private float xOffset  = 0.0f;
+    private float yValue = 0.0f;
+    private Vector2 screenCenter;
+    private Vector2 basePosition;
     private bool increment;
     public override void _Ready()
     {
+        GD.Randomize();
+        sinePattern = new();
         SetAnchorsPreset(LayoutPreset.Center);
         Position = GetViewportRect().Size / 2f;
+        basePosition = Position;
     }
 
     public override void _Draw()
@@ -29,12 +38,45 @@ public partial class Crosshair : Control
         );
     }
 
+    public void RequestReset()
+    {
+        sinePattern.SetCurrentPattern(SinePatterns.Patterns.RegularSine);
+        Position = basePosition;
+        yValue = xOffset = 0.0f;
+        //direction = GD.Randf() < 0.5f ? -1 : 1;
+        Visible = false;
+    }
+
+    public void RequestPatternLoad(SinePatterns.Patterns newPattern)
+    {   
+        sinePattern.SetCurrentPattern(newPattern);
+        Visible = true;
+    }
+
     public override void _Process(double delta)
     {
+
         base._Process(delta);
-        if(Radius >= 80)
+
+        if(!Visible)
+            return;
+
+        CrosshairGrowShrink();
+        
+
+        // Move Y back and forth
+        sinePattern.ProcessCurrentPattern(ref yValue, ref xOffset, delta);
+
+        Position = screenCenter + new Vector2(xOffset + basePosition.X, yValue + basePosition.Y);
+
+        QueueRedraw();
+    }
+
+    private void CrosshairGrowShrink()
+    {
+        if(Radius >= 90)
             increment = false;
-        else if(Radius <= 20)
+        else if(Radius <= 30)
             increment = true;
         
 
@@ -42,7 +84,7 @@ public partial class Crosshair : Control
             Radius += 1;
         else
             Radius -= 1;
-
-        QueueRedraw();
     }
+
+
 }
