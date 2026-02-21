6ac5033d8dac689e4252cd607bcb7159b96cab15
Robert Baumgarten <robbiefb1119@gmail.com>
2026-02-20 21:13:39 -0500

    Repurposed funny scene

--- Do not look here/Nothing to see here/IM WARNING YOU/Alright, you asked for this/SecretScene.cs
+++ Do not look here/Nothing to see here/IM WARNING YOU/Alright, you asked for this/SecretScene.cs
@@ -3,15 +3,12 @@ using System;
 
 public partial class SecretScene : Node3D
 {
-    [Export] public AudioStreamPlayer2D audio;
     [Export] public Button back;
 
 
     public override void _Ready()
     {
         base._Ready();
-
-        audio.Play();
     }
 
     public void _on_back_pressed()
